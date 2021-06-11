96dcdc84365fbc129343b41a63b44c15b7c4d276
mibregeth <43907401+Maen22@users.noreply.github.com>
2021-06-11 20:30:02 +0300

    added CommentUpdated Event logic.

--- comments/index.js
+++ comments/index.js
@@ -33,8 +33,22 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Recieved Event', req.body.type)
+
+  const { type, data, status } = req.body
+
+  if (type === 'CommentModerated') {
+    const { id, postId } = data
+    const comments = commentsByPostId[postId]
+    const comment = comments.find((c) => c.id === id)
+    comment.status = status
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: { ...comment, postId },
+    })
+  }
 
   res.send({})
 })
